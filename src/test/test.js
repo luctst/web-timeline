c50aeec78d7ca3f549eefe5fed2cb5a871557ea4
luctst <luc.tostee@outlook.fr>
2018-11-13 17:34:21 +0100

    Connect spreadsheep API to our webpage

--- src/test/test.js
+++ src/test/test.js
@@ -2,26 +2,36 @@
  * Variables
  */
 const spreadsheetsId = `1xG2xF92GiSf5yVHU5JFoEHrAvR2ksaMNm7kMHAI4Iyg`;
-const spreadsheet = `"https://spreadsheets.google.com/feeds/list/${spreadsheetsId}/1/public/values?alt=json";`;
+const spreadsheet = `https://spreadsheets.google.com/feeds/list/${spreadsheetsId}/1/public/values?alt=json`;
 const app = document.getElementById("app");
+const gsx = `gsx$`;
 
 /**
  * Déclaration
  */
-// const api = async () => {
-//     let data = await spreadsheet;
-//     let res = await data.json();
-//     console.log(res);
-// };
-
-const api = () => {
-    fetch(spreadsheet)
-        .then(
-        res => {
-            // res.json();
-            console.log(res);
-        }
-    )
+const render = (array) => {
+    array.forEach(el => {
+        const div = document.createElement("div");
+        const h1 = document.createElement("h1");
+        const p = document.createElement("p");
+        const hr = document.createElement("hr");
+        h1.innerHTML += `${el.gsx$id.$t} - ${el.gsx$title.$t}`;
+        p.innerHTML += `<strong>${el.gsx$description.$t}</strong>`;
+        div.appendChild(h1);
+        div.appendChild(p);
+        div.appendChild(hr);
+        app.appendChild(div);
+    });
+}
+const api = async () => {
+    try {
+        let data = await fetch(spreadsheet);
+        let res = await data.json();
+        console.log(res.feed.entry);
+        render(res.feed.entry);
+    } catch (error) {
+        
+    }
 };
 
 /**
