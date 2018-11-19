025e9224584668f91072ae4469f6ddb9704b0bd9
luctst <luc.tostee@outlook.fr>
2018-11-20 00:48:40 +0100

    Add AJAX call

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const navbar = document.getElementById("js-stickyMenu");
 const spreadsheetsId = `1xG2xF92GiSf5yVHU5JFoEHrAvR2ksaMNm7kMHAI4Iyg`;
 const spreadsheet = `https://spreadsheets.google.com/feeds/list/${spreadsheetsId}/1/public/values?alt=json`;
 const app = document.getElementById("app");
-const gsx = `gsx$`;
 
 /**
  * Déclaration
@@ -18,15 +17,38 @@ const navFixed = () => { // TODO: Fixed navbar
     }
 }
 const render = (array) => {
-    const div = document.createElement("div");
-    const p = document.createElement("p");
-    div.classList.add("content");
-    app.append(div);
+    array.forEach(el => {
+        const div = document.createElement("div");
+        div.classList.add("content");
+        div.innerHTML += `
+            <div class="content--date">
+                <p class="is--text--content">${el.gsx$date.$t}</p>
+            </div>
+            <div class="content--cat">
+                <p class="is--text--content">${el.gsx$title.$t}</p>
+            </div>
+            <div class="content--title">
+                <p class="is--text--content">${el.gsx$description.$t}</p>
+            </div>
+            <div class="content--more">
+                <p><button type="button" class="is__btn">See more !</button></p>
+            </div>`;
+        app.appendChild(div);
+    });
+}
+const getData = async () => {
+    try {
+        let data = await fetch(spreadsheet);
+        let res = await data.json();
+        render(res.feed.entry);
+    } catch (error) {
+        
+    }
 }
 
 
 /**
  * Éxecution
 */
-// window.onscroll = function () { myFunction() };
+document.addEventListener("DOMContentLoad", getData());
 window.addEventListener("scroll", navFixed);
