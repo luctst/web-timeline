d9f173959015259b5f9516a7cbe7490a0006a954
luctst <luc.tostee@outlook.fr>
2018-11-20 11:16:50 +0100

    Fixed navbar bug, and change data

--- src/main.js
+++ src/main.js
@@ -5,18 +5,19 @@ const navbar = document.getElementById("js-stickyMenu");
 const spreadsheetsId = `1xG2xF92GiSf5yVHU5JFoEHrAvR2ksaMNm7kMHAI4Iyg`;
 const spreadsheet = `https://spreadsheets.google.com/feeds/list/${spreadsheetsId}/1/public/values?alt=json`;
 const app = document.getElementById("app");
+const sticky = navbar.offsetTop;
 
 /**
  * Déclaration
  */
 const navFixed = () => { // TODO: Fixed navbar
-    if (window.pageYOffset >= navbar.offsetTop) {
+    if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
     }
 }
-const render = (array) => {
+const render = (array) => { // TODO: Display content
     array.forEach(el => {
         const div = document.createElement("div");
         div.classList.add("content");
@@ -25,7 +26,7 @@ const render = (array) => {
                 <p class="is--text--content">${el.gsx$date.$t}</p>
             </div>
             <div class="content--cat">
-                <p class="is--text--content">${el.gsx$title.$t}</p>
+                <p class="is--text--content">${el.gsx$category.$t}</p>
             </div>
             <div class="content--title">
                 <p class="is--text--content">${el.gsx$description.$t}</p>
@@ -36,7 +37,7 @@ const render = (array) => {
         app.appendChild(div);
     });
 }
-const getData = async () => {
+const getData = async () => { // TODO: Retrieve data
     try {
         let data = await fetch(spreadsheet);
         let res = await data.json();
