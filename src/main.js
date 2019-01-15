a225743c901934e098b8b25084bb34cae139c2a5
luctst <luc.tostee@outlook.fr>
2019-01-15 12:57:40 +0100

    Use object to create element and refactorize code

--- src/main.js
+++ src/main.js
@@ -7,86 +7,72 @@ const spreadsheet = `https://spreadsheets.google.com/feeds/list/${spreadsheetsId
 const app = document.getElementById("app");
 const sticky = navbar.offsetTop;
 const select = document.querySelector("select");
+const elementTab = [];
+const dateField = document.querySelector(".is__subTitle");
 
 /**
  * Déclaration
  */
+const getData = async bdd => { // TODO: Get data from BDD
+    try {
+        let data = await fetch(bdd);
+        let res = await data.json();
+        return res.feed.entry;
+    } catch (error) {
+        throw error;
+    }
+}
+const renderContent = async (index) => { // TODO: Render element with unknown index
+    let data = await getData(spreadsheet);
+    for (let i = 0; i < index; i++) {
+        let el = new Element();
+        el.createElement(data[i]);
+        el.getDescription();
+        elementTab.push(el);
+    }
+    console.log(elementTab);
+}
+class Element { // TODO: Class for new Element
+    createElement(el) {
+        const div = document.createElement("div");
+        div.classList.add("content");
+        div.innerHTML += `
+            <div class="content--date">
+                <p class="is__text__content">${el.gsx$date.$t}</p>
+            </div>
+            <div class="content--cat">
+                <p class="is__text__content">${el.gsx$category.$t}</p>
+            </div>
+            <div class="content--title">
+                <p class="is__text__content">${el.gsx$title.$t}</p>
+            </div>
+            <div class="content--more">
+                <p><button type="button" class="is__btn">See more !</button></p>
+            </div>
+            <div class="content--description is__none">
+                <p class="is__text__content">${el.gsx$description.$t}</p>
+                <p class="is__text__content"><a href="${el.gsx$article.$t}" target=_blank>${el.gsx$article.$t}</a></p>
+            </div>`;
+        app.appendChild(div);
+        this.html = div;
+    };
+    getDescription() {
+        const elementParent = this.html;
+        const lastChild = elementParent.lastChild;
+        elementParent.addEventListener("click", () => {
+            if (lastChild.className === "content--description is__none") {
+                lastChild.classList.remove("is__none");
+            } else {
+                lastChild.classList.add("is__none");
+            }
+        });
+    }
+}
 const navFixed = () => { // TODO: Fixed navbar
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
-    }
-}
-const createContent = el => { // TODO: Create content
-    const div = document.createElement("div");
-    if (el.gsx$important.$t) {
-        div.classList.add("is__content__important");
-        div.innerHTML += `
-                <div class="content--date">
-                    <p class="is__text__content">${el.gsx$date.$t}</p>
-                </div>
-                <div class="content--cat">
-                    <p class="is__text__content">${el.gsx$category.$t}</p>
-                </div>
-                <div class="content--title">
-                    <p class="is__text__content">${el.gsx$title.$t}</p>
-                </div>
-                <div class="content--more">
-                    <p><button type="button" class="is__btn__important">See more !</button></p>
-                </div>
-                <div class="content--description is__none">
-                    <p class="is__text__content">${el.gsx$description.$t}</p>
-                    <p class="is__text__content"><a href="${el.gsx$article.$t}" target=_blank>${el.gsx$article.$t}</a></p>
-                </div>`;
-        app.appendChild(div);
-    } else {
-        div.classList.add("content");
-        div.innerHTML += `
-                <div class="content--date">
-                    <p class="is__text__content">${el.gsx$date.$t}</p>
-                </div>
-                <div class="content--cat">
-                    <p class="is__text__content">${el.gsx$category.$t}</p>
-                </div>
-                <div class="content--title">
-                    <p class="is__text__content">${el.gsx$title.$t}</p>
-                </div>
-                <div class="content--more">
-                    <p><button type="button" class="is__btn">See more !</button></p>
-                </div>
-                <div class="content--description is__none">
-                    <p class="is__text__content">${el.gsx$description.$t}</p>
-                    <p class="is__text__content"><a href="${el.gsx$article.$t}" target=_blank>${el.gsx$article.$t}</a></p>
-                </div>`;
-        app.appendChild(div);
-    }
-};
-const renderFirstContent = async () => { // TODO: Render content
-    let windowSize = window.innerHeight;
-    try {
-        let data = await fetch(spreadsheet);
-        let res = await data.json();
-        let entry = res.feed.entry;
-        if (windowSize <= 767) {
-            for (let i = 0; i < entry.length; i++) {
-                if (i <= 5) {
-                    createContent(entry[i]);
-                } else if (i > 5) {
-                    break;
-                }
-            }
-        } else {
-            for (let i = 0; i < entry.length; i++) {
-                if (i <= 10) {
-                    createContent(entry[i]);
-                } else if (i > 10) {
-                    break;
-                }
-            }
-        }
-    } catch (error) {
-        throw error;
     }
 }
 const addContent = async () => { // TODO: Add content when scroll bottom of the page.
@@ -97,10 +83,10 @@ const addContent = async () => { // TODO: Add content when scroll bottom of the 
         let contentDate = document.querySelectorAll(".content--date");
         for (let i = 0; contentDate.length <= res.length; i++) {
             let getLastDate = contentDate.item(contentDate.length - 1).firstElementChild.textContent;
-                if (res[i].gsx$date.$t === getLastDate) {
-                    let id = parseInt(res[i].gsx$id.$t);
-                    for (let y = id; y <= id + 5; y++) {
-                        createContent(res[y]);
+            if (res[i].gsx$date.$t === getLastDate) {
+                let id = parseInt(res[i].gsx$id.$t);
+                for (let y = id; y <= id + 5; y++) {
+                    createContent(res[y]);
                 }
             }
         }
@@ -126,24 +112,11 @@ const filter = async () => { // TODO: Filter event by tag.
         throw error;
     }
 };
-const getDescription = element => {
-    if (element.target.className === "is__btn" || element.target.className === "is__btn__important") {
-        let parent = element.target.parentNode.parentNode.parentNode;
-        let childLast = parent.lastElementChild;
-        const classNom = "content--description is__none";
-        if (childLast.className === classNom) {
-            childLast.classList.remove("is__none");
-        } else {
-            childLast.classList.add("is__none");
-        }
-    }
-}
 
 /**
  * Éxecution
  */
-window.addEventListener("DOMContentLoaded", renderFirstContent);
-window.addEventListener("click", (event) => getDescription(event));
+window.addEventListener("DOMContentLoaded", () => renderContent(5));
 window.addEventListener("scroll", () => {
     navFixed();
     if (select.value === "") {
@@ -153,3 +126,8 @@ window.addEventListener("scroll", () => {
     };
 });
 select.addEventListener("change", filter);
+dateField.addEventListener("click", () => {
+    app.innerHTML = "";
+    elementTab.reverse();
+    console.log(elementTab);
+});
