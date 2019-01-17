513042f23e07759fbbf2f897fdbb0eaa9178570e
luctst <luc.tostee@outlook.fr>
2019-01-17 18:43:50 +0100

    Update code

--- src/main.js
+++ src/main.js
@@ -99,14 +99,14 @@ class Element { // TODO: Class for new Element
                 <p class="is__text__content">${el.gsx$category.$t}</p>
             </div>
             <div class="content--title">
-                <p class="is__text__content">${el.gsx$title.$t}</p>
+                <p class="is__title__content">${el.gsx$title.$t}</p>
             </div>
             <div class="content--more">
-                <img class="is__btn" src="./img/arrow.svg" alt="Cliquez pour voir la description"/>
+                <img class="is__img__arrow" src="./img/arrow.svg" alt="Cliquez pour voir la description"/>
             </div>
             <div class="content--description is__none">
-                <p class="is__text__content">${el.gsx$description.$t}</p>
-                <p class="is__text__content"><a href="${el.gsx$article.$t}" target=_blank>${el.gsx$article.$t}</a></p>
+                <p class="is__title__content">${el.gsx$description.$t}</p>
+                <p class="is__btn"><a href="#">See more</a></p>
             </div>`;
         app.appendChild(div);
         this.html = div;
@@ -118,10 +118,10 @@ class Element { // TODO: Class for new Element
         elementParent.addEventListener("click", () => {
             if (lastChild.className === "content--description is__none") {
                 lastChild.classList.remove("is__none");
-                elementParent.querySelector(".is__btn").style.transform = "rotate(180deg)";
+                elementParent.querySelector(".is__img__arrow").style.transform = "rotate(180deg)";
             } else {
                 lastChild.classList.add("is__none");
-                elementParent.querySelector(".is__btn").style.transform = "";
+                elementParent.querySelector(".is__img__arrow").style.transform = "";
             }
         });
     }
