1299f585585d1e8571bcf557eae5fbaa9958f362
Kévin Joya <kevin.joya@outlook.com>
2019-10-14 15:10:16 +0200

    Trying display right icons

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from 'styled-components'
 import "../styles/main.scss";
-import Network from "../assets/img/Network.svg";
-import Launch from "../assets/img/Launch.svg";
-import Ai from "../assets/img/Ai.svg";
-import arrow from "../assets/img/arrow.svg";
-import Programming from "../assets/img/Programming.svg";
-import Science from "../assets/img/Science.svg";
-import Security from "../assets/img/Security.svg";
-import SocialMedia from "../assets/img/Social-Media.svg";
-import Design from "../assets/img/Design.svg";
+// import Network from "../assets/img/Network.svg";
+// import Launch from "../assets/img/Launch.svg";
+// import Ai from "../assets/img/Ai.svg";
+// import arrow from "../assets/img/arrow.svg";
+// import Programming from "../assets/img/Programming.svg";
+// import Science from "../assets/img/Science.svg";
+// import Security from "../assets/img/Security.svg";
+// import SocialMedia from "../assets/img/Social-Media.svg";
+// import Design from "../assets/img/Design.svg";
 
 function Event(props) {
   const Date = styled.h3`
@@ -20,11 +20,11 @@ function Event(props) {
         <div className="main--left--element--date">
           <Date className="is__date">{props.gsx$date.$t}</Date>
           {/* <h3 className="is__date">{props.gsx$date.$t}</h3> */}
-          {/* <p>20/10/2019</p> */}
         </div>
         <div className="main--left--element--img">
           {/* <img src="../assets/img/Network.svg" alt="Icon"></img> */}
-          <img src={Network} alt="Icon" className="is__element__img" width="60px"></img>
+          <img src={`static/media/${props.gsx$category.$t}.svg`} alt="Icon" className="is__element__img" width="60px"></img>
+          {/* <img src={Network} alt="Icon" className="is__element__img" width="60px"></img> */}
         </div>
         <div className="main--left--element--text">
           <h2 className="is__title__element">{props.gsx$title.$t}</h2>
