a6b4a5016f84bbc993b8f18dab304d61dbec534e
Kévin Joya <kevin.joya@outlook.com>
2019-10-14 10:43:28 +0200

    Added data and style

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from 'styled-components'
 import "../styles/main.scss";
 import Network from "../assets/img/Network.svg";
 import Launch from "../assets/img/Launch.svg";
@@ -10,20 +11,25 @@ import Security from "../assets/img/Security.svg";
 import SocialMedia from "../assets/img/Social-Media.svg";
 import Design from "../assets/img/Design.svg";
 
-function Event() {
+function Event(props) {
+  const Date = styled.h3`
+    margin-top: 18px;
+  `
     return (
       <React.Fragment>
         <div className="main--left--element--date">
-          <p>20/10/2019</p>
+          <Date className="is__date">{props.gsx$date.$t}</Date>
+          {/* <h3 className="is__date">{props.gsx$date.$t}</h3> */}
+          {/* <p>20/10/2019</p> */}
         </div>
         <div className="main--left--element--img">
           {/* <img src="../assets/img/Network.svg" alt="Icon"></img> */}
           <img src={Network} alt="Icon" className="is__element__img" width="60px"></img>
         </div>
         <div className="main--left--element--text">
-          <h2 className="is__title__element">Title</h2>
-          <p className="is__content__element">description</p>
-          <p className="is__content__tag__element">#category</p>
+          <h2 className="is__title__element">{props.gsx$title.$t}</h2>
+          <p className="is__content__element">{props.gsx$description.$t}</p>
+          <p className="is__content__tag__element">#{props.gsx$category.$t}</p>
         </div>
       </React.Fragment>
     )
