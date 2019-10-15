9f1b90b4c4f1f7f943e8d0f03b1592219e8b131c
Kévin Joya <kevin.joya@outlook.com>
2019-10-15 16:55:26 +0200

    Changed order data

--- src/container/Timeline.js
+++ src/container/Timeline.js
@@ -3,10 +3,10 @@ import Event from "../components/Event";
 import Filters from './Filters';
 
 function Timeline() {
-  const filterImg = React.createRef();
+  // const filterImg = React.createRef();
 
   const [state, setState] = useState({data: null})
-  const [orderByDesc, setOder] = useState(false); // Use this for ordering your `state.data`
+  const [orderByDesc, setOrder] = useState(false); // Use this for ordering your `state.data`
 
   useEffect(() => {
     fetch(`https://spreadsheets.google.com/feeds/list/${process.env.REACT_APP_KEY}/1/public/values?alt=json`)
@@ -19,34 +19,36 @@ function Timeline() {
     console.log(state.data);
   }, []);
 
-  const changeOrder = () => {
-    if (filterImg.current.style.transform === '') {
-      filterImg.current.style.transform = 'rotate(180deg)'
-        const newState = {...state}
-        state.data.sort((a, b) => a-b).reverse()
-        setState(newState)
-    } else if(filterImg.current.style.transform === 'rotate(180deg)') {
-          filterImg.current.style.transform = '';
-          const newState = {...state}
-          state.data.sort((a, b) => a + b).reverse()
-          setState(newState)
-        }
-    setOder(!orderByDesc); // Everytime you click on <p>sort by date</p> state should be update
-  }
-
   return(
     <React.Fragment>
-      <Filters ref={filterImg} changeDataOrder={changeOrder} />
-
-      {state.data === null ? "loading" :
-       state.data.map((event, index) => {
-        return (
-          <div className="main--left--element" key={index}>
-            <Event gsx$date={event.gsx$date} gsx$title={event.gsx$title} gsx$description={event.gsx$description} gsx$category={event.gsx$category} key={index} />
-         </div>
-        )
-      })
-    }
+      <Filters changedDataOrder={() => setOrder(!orderByDesc)} />
+      {
+        function () {
+          if (state.data === null) {
+            return "loading"
+          } else {
+            if (!orderByDesc) {
+              state.data.sort((a, b) => a + b).reverse();
+              return state.data.map((event, index) => {
+                return (
+                  <div className="main--left--element" key={index}>
+                    <Event gsx$date={event.gsx$date} gsx$title={event.gsx$title} gsx$description={event.gsx$description} gsx$category={event.gsx$category} key={index} />
+                  </div>
+                )
+              })
+            } else if(orderByDesc) {
+              state.data.sort((a, b) => a - b).reverse();
+              return state.data.map((event, index) => {
+                return (
+                  <div className="main--left--element" key={index}>
+                    <Event gsx$date={event.gsx$date} gsx$title={event.gsx$title} gsx$description={event.gsx$description} gsx$category={event.gsx$category} key={index} />
+                </div>
+                )
+              })
+            }
+          }
+        }()
+      }
     </React.Fragment>
   )
 }
