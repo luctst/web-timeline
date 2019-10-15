9da283cfa44c4f26dbf139492a3ad5228d29b0c7
Kévin Joya <kevin.joya@outlook.com>
2019-10-15 16:59:40 +0200

    Added prop data

--- src/components/Event.js
+++ src/components/Event.js
@@ -18,18 +18,18 @@ function Event(props) {
     return (
       <React.Fragment>
         <div className="main--left--element--date">
-          <Date className="is__date">{props.gsx$date.$t}</Date>
+          <Date className="is__date">{props.data.gsx$date.$t}</Date>
           {/* <h3 className="is__date">{props.gsx$date.$t}</h3> */}
         </div>
         <div className="main--left--element--img">
           {/* <img src="../assets/img/Network.svg" alt="Icon"></img> */}
-          <img src={`static/media/${props.gsx$category.$t}.svg`} alt="Icon" className="is__element__img" width="60px"></img>
+          <img src={`static/media/${props.data.gsx$category.$t}.svg`} alt="Icon" className="is__element__img" width="60px"></img>
           {/* <img src={Network} alt="Icon" className="is__element__img" width="60px"></img> */}
         </div>
         <div className="main--left--element--text">
-          <h2 className="is__title__element">{props.gsx$title.$t}</h2>
-          <p className="is__content__element">{props.gsx$description.$t}</p>
-          <p className="is__content__tag__element">#{props.gsx$category.$t}</p>
+          <h2 className="is__title__element">{props.data.gsx$title.$t}</h2>
+          <p className="is__content__element">{props.data.gsx$description.$t}</p>
+          <p className="is__content__tag__element">#{props.data.gsx$category.$t}</p>
         </div>
       </React.Fragment>
     )
