d6f0a2f5be6063d6d4c76c524d253538861aa3c8
Kévin Joya <kevin.joya@outlook.com>
2019-10-15 12:06:50 +0200

    Trying something

--- src/container/Filters.js
+++ src/container/Filters.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import SortByDate from "../assets/img/sort-by-date.svg";
 import Arrow from "../assets/img/arrow.svg";
 
-const Filters = () => {
+const Filters = (props) => {
+  const [state, setState] = useState({
+    isOrderDesc: true
+  })
+  const filterImg = useRef(null);
+
+  const handleClick = () => {
+    // console.log(props.data);
+    // state.filterData = props.data
+    console.log(props);
+    if (filterImg.current.style.transform === '') {
+      const newState = {...state}
+      newState.isOrderDesc = true
+      filterImg.current.style.transform = 'rotate(180deg)'
+      props.data.sort((a, b) => a-b).reverse()
+      setState(newState)
+
+    } else if(filterImg.current.style.transform === 'rotate(180deg)') {
+      const newState = {...state}
+      filterImg.current.style.transform = '';
+      newState.isOrderDesc = false
+      // console.log(props);
+      // return (props.data.sort((a, b) => a-b).reverse())
+      props.data.sort((a, b) => a + b).reverse()
+      setState(newState)
+    }
+  }
+
   return (
     <React.Fragment>
       <div className="header--infobar">
-        <div className="header--infobar--date">
+        <div className="header--infobar--date" onClick={handleClick}>
           <img src={SortByDate} alt="icon"></img>
           <p className="is__sort__date">Sort by <span>Date</span></p>
-          <img src={Arrow} width="20px" alt="icon"></img>
+          <img src={Arrow} width="20px" alt="icon" ref={filterImg}></img>
         </div>
         <div className="header--infobar--categories">
           <select className="is__select is__subTitle">
