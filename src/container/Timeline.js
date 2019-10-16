4edbd21a1f628be3b5672c7504bfe0017c2553f8
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-17 01:00:16 +0200

    Re organize diferents states, update code to be more readable, create handleCha,geCategory fn

--- src/container/Timeline.js
+++ src/container/Timeline.js
@@ -3,65 +3,40 @@ import Event from "../components/Event";
 import Filters from './Filters';
 
 function Timeline() {
-  const [state, setState] = useState({
-    data: null,
-    filter: {
-      category: ""
-    }
-  })
-  const [orderByDesc, setOrder] = useState(false); // Use this for ordering your `state.data`
+  const [data, setData] = useState([])
+  const [orderByDesc, setOrder] = useState(false); // Use this for ordering your `data.data`
+  const [filters, setFilters] = useState({
+    category: "Network"
+  });
 
   useEffect(() => {
     fetch(`https://spreadsheets.google.com/feeds/list/${process.env.REACT_APP_KEY}/1/public/values?alt=json`)
     .then(response => response.json())
-    .then(dataParsed => {
-      const newState = dataParsed.feed.entry;
-      console.log(newState[1]);
-      setState({data: newState, filter: {category: ""} });
-    })
-    console.log(state.data);
+    .then(dataParsed => setData([...dataParsed.feed.entry]))
   }, []);
 
   function handleChangeCategory(el) {
-    console.log(el.target.value);
-    const newState = {...state};
-    console.log(newState.filter.category);
-    newState.filter.category = el.target.value;
-    console.log(newState.filter.category);
-    console.log(newState);
-    setState(newState);
+    if (el.target.value === filters.category) return null;
+
+    const newState = {...filters};
+    newState.category = el.target.value;
+
+    setFilters(newState);
   }
 
   return(
     <React.Fragment>
       <Filters changedDataOrder={() => setOrder(!orderByDesc)} changedCategory={handleChangeCategory} />
-      {/* <Filters changedDataOrder={() => setOrder(!orderByDesc)} /> */}
       {
-        function () {
-          if (state.data === null) {
-            return "loading"
-          } else {
-            if (orderByDesc) {
-              state.data.sort((a, b) => a + b).reverse();
-              return state.data.map((event, index) => {
-                return (
-                  <div className="main--left--element" key={index}>
-                    <Event data={event} key={index} />
-                  </div>
-                )
-              })
-            } else if(!orderByDesc) {
-              state.data.sort((a, b) => a - b).reverse();
-              return state.data.map((event, index) => {
-                return (
-                  <div className="main--left--element" key={index}>
-                    <Event data={event} key={index} />
-                  </div>
-                )
-              })
-            }
+        (function () {
+          if (data.length === 0) return "Loading..";
+
+          if (orderByDesc) {
+            return data.reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index}/>)
           }
-        }()
+
+          return data.map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />)
+        })()
       }
     </React.Fragment>
   )
