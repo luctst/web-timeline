03d71e7b034889fefd98206144fb038dc41130e8
Kévin Joya <kevin.joya@outlook.com>
2019-10-17 15:53:35 +0200

    Added right code for overview

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from 'styled-components'
 import "../styles/main.scss";
+import EventOverview from "./EventOverview";
 // import Network from "../assets/img/Network.svg";
 // import Launch from "../assets/img/Launch.svg";
 // import Ai from "../assets/img/Ai.svg";
@@ -12,23 +13,36 @@ import "../styles/main.scss";
 // import Design from "../assets/img/Design.svg";
 
 function Event(props) {
+  const hiddenOverview = useRef(null);
   const Date = styled.h3`
     margin-top: 18px;
   `
+
+  const toggleClass = () => {
+    document.body.classList.toggle("is__overflow__hidden");
+    hiddenOverview.current.classList.toggle("is__none");
+  }
+
   return (
-    <div className="main--left--element">
-      <div className="main--left--element--date">
-        <Date className="is__date">{props.data.gsx$date.$t}</Date>
+    <React.Fragment>
+      <div className="main--left--element" onClick={toggleClass}>
+        <div className="main--left--element--date">
+          <Date className="is__date">{props.data.gsx$date.$t}</Date>
+        </div>
+        <div className="main--left--element--img">
+          <img src={props.data.gsx$category.$t} alt="Icon" className="is__element__img" width="60px"></img>
+        </div>
+        <div className="main--left--element--text">
+          <h2 className="is__title__element">{props.data.gsx$title.$t}</h2>
+          <p className="is__content__element">{props.data.gsx$description.$t}</p>
+          <p className="is__content__tag__element">#{props.data.gsx$category.$t}</p>
+        </div>
       </div>
-      <div className="main--left--element--img">
-        <img src={props.data.gsx$category.$t} alt="Icon" className="is__element__img" width="60px"></img>
-      </div>
-      <div className="main--left--element--text">
-        <h2 className="is__title__element">{props.data.gsx$title.$t}</h2>
-        <p className="is__content__element">{props.data.gsx$description.$t}</p>
-        <p className="is__content__tag__element">#{props.data.gsx$category.$t}</p>
-      </div>
-    </div>
+      <section className="main--right is__none" ref={hiddenOverview}>
+        <EventOverview data={props.data} closeOverview={toggleClass} />
+        {/* <EventOverview data={props.data} closeOverview={arrowClose}  /> */}
+      </section>
+    </React.Fragment>
   )
 }
 
