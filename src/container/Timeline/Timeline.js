7b06c39340d6f146df4366faefedfaf7361d8c67
Kévin Joya <kevin.joya@outlook.com>
2019-10-22 17:55:59 +0200

    Try something for search bar

--- src/container/Timeline/Timeline.js
+++ src/container/Timeline/Timeline.js
@@ -2,12 +2,17 @@ import React, {useState, useEffect} from "react";
 import Event from "../../components/Event/Event";
 import Filters from '../Filters/Filters';
 import Loader from "../../components/Loader/Loader";
+import { fdatasync } from "fs";
 
 function Timeline() {
   const [data, setData] = useState([])
   const [orderByDesc, setOrder] = useState(false); // Use this for ordering your `data.data`
   const [filters, setFilters] = useState({
-    category: "Network"
+    category: "Network",
+    showEvents: {
+      showList: true,
+      searchInput: ""
+    }
   });
 
   useEffect(() => {
@@ -25,17 +30,69 @@ function Timeline() {
     setFilters(newState);
   }
 
+  function handleOnChange(e) {
+  //   console.log(e.target.value);
+  //   console.log(filters.showEvents.searchInput);
+
+    if(e.target.value === filters.showEvents.searchInput) return null;
+
+    // let newData = {...data};
+    let newData = [];
+    console.log(newData);
+
+    const newState = {...filters}
+    newState.showEvents.searchInput = e.target.value
+
+    console.log(newState);
+    data.map(event => {
+      if (event.gsx$title.$t.includes(newState.showEvents.searchInput)) {
+        console.log(event);
+        newData.push(event)
+        // setData([...newData, event]);
+        console.log("------------------------------------------------");
+        console.log(newData);
+
+        setData(newData);
+      }
+    })
+    console.log(data);
+  }
+
   return(
     <React.Fragment>
-      <Filters changedDataOrder={() => setOrder(!orderByDesc)} changedCategory={handleChangeCategory} />
+      {/* <Filters changedDataOrder={() => setOrder(!orderByDesc)} changedCategory={handleChangeCategory} /> */}
+      <Filters changedDataOrder={() => setOrder(!orderByDesc)} changedCategory={handleChangeCategory} handleChangedSearchInput={e => handleOnChange(e)}/>
       <section className="main--left">
         {
           (function () {
             if (data.length === 0) return <Loader />;
+
             if (orderByDesc) {
-              return data.sort((a,b) => a-b).reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />)
+              if (filters.showEvents.showList) {
+                return data.sort((a,b) => a-b).reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />)
+              }
             }
-            return data.sort((a,b) => a+b).reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />)
+            if (!orderByDesc) {
+              if (filters.showEvents.showList) {
+                  return data.sort((a,b) => a+b).reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />)
+                // return data.map(e => {
+                //   return e.gsx$title.$t.includes(filters.showEvents.showList.searchInput) ?
+                //   data.sort((a,b) => a+b).reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />) :
+                //   null
+                // })
+
+              }
+            }
+            // if (!orderByDesc) {
+            //   if (!filters.showEvents.showList) {
+            //     return data.map(e => {
+            //       return e.gsx$title.$t.includes(filters.showEvents.showList.searchInput) ?
+            //       data.sort((a,b) => a+b).reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />) :
+            //       null
+            //     })
+            //     // return data.sort((a,b) => a+b).reverse().map((event, index) => filters.category === event.gsx$category.$t && <Event data={event} key={index} />)
+            //   }
+            // }
           })()
         }
       </section>
