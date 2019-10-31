9f2a8fb033a87be613ca92f712415e716938863b
Kévin Joya <kevin.joya@outlook.com>
2019-10-31 16:08:32 +0100

    Added style for select filter search

---
+++ src/container/SearchBar/SelectStyled.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const SelectStyled = styled.select `
+  color: #333;
+  background: none;
+  border: none;
+  border-radius: 0;
+  font-size: 1.6rem;
+`
+
+export default SelectStyled
