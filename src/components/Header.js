5cdce345212af2ab15671c9e1ae2f0e3d38cc034
Kévin Joya <kevin.joya@outlook.com>
2019-10-31 17:28:48 +0100

    Added mt

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className="header--title">
-            <h1 className="is__main__title">The Evolution of Technologies</h1>
+            <h1 className="is__main__title mt-5">The Evolution of Technologies</h1>
         </div>
     )
 }
