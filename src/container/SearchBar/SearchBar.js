291de6980632a1337f7dc8d3dcce9358bc156cba
Kévin Joya <kevin.joya@outlook.com>
2019-10-31 16:00:03 +0100

    Remove dropdown menu with select input

--- src/container/SearchBar/SearchBar.js
+++ src/container/SearchBar/SearchBar.js
@@ -1,17 +1,15 @@
 import React from "react";
 import SearchBarStyled from "./SearchBarStyled.style";
+import SelectStyled from "./SelectStyled.style";
 
 const SearchBar = (props) => {
   return (
     <SearchBarStyled className="input-group mb-3 search--bar">
       <div className="input-group-prepend">
-        <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Filters</button>
-        <div className="dropdown-menu">
-          <ul className="dropdown--item--ul">
-            <li className="dropdown--item--li"> <a href="#/" className="dropdown--item--a">Title</a></li>
-            <li className="dropdown--item--li"> <a href="#/" className="dropdown--item--a">Description</a></li>
-          </ul>
-        </div>
+        <SelectStyled className="form-control" id="filters--SelectStyled" onChange={props.searchFilter}>
+          <option>Title</option>
+          <option>Description</option>
+        </SelectStyled>
         <input type="text" className="form-control" aria-label="Text input with dropdown button" placeholder="Search" onChange={props.handleChangedSearchInput} />
       </div>
     </SearchBarStyled>
