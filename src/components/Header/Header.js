b147af6a17ead294757f137e9ed560e67be30b6d
Kévin Joya <kevin.joya@outlook.com>
2019-11-04 11:44:43 +0100

    created styled compo for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderStyled from "./HeaderStyled.style";
+
+const Header = () => {
+    return (
+        <div className="header--title">
+            <HeaderStyled className="is__main__title mt-5">The Evolution of Technologies</HeaderStyled>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Header/HeaderStyled.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.h1 `
+  color: $contentColor;
+  font-family: "helvetica Neue";
+  line-height: 0.7em;
+  font-size: 3rem;
+  font-weight: 300;
+`
+
+export default HeaderStyled;
