c51c628480feac8cd258f4cd8b4ea773c116c349
Kévin Joya <kevin.joya@outlook.com>
2019-11-07 15:08:10 +0100

    Added compo for fitlers style

--- src/container/Timeline/FiltersStickyStyled.style.js
+++ src/container/Timeline/FiltersStickyStyled.style.js
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 
 const FiltersStickyStyled = styled.div `
-  .sticky {
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 10;
-    min-height: 5rem;
-  }
+  position: fixed;
+  top: 10%;
+  width: 69%;
+  z-index: 10;
+  min-height: 5rem;
 `
 
 export default FiltersStickyStyled
