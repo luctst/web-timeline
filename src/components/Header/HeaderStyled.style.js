e15ce11fc8fb276ddb516bec01ee462beb43cc19
Kévin Joya <kevin.joya@outlook.com>
2019-11-07 15:07:23 +0100

    Added position fixed for header

--- src/components/Header/HeaderStyled.style.js
+++ src/components/Header/HeaderStyled.style.js
@@ -6,6 +6,11 @@ const HeaderStyled = styled.h1 `
   line-height: 0.7em;
   font-size: 3rem;
   font-weight: 300;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1000;
+  min-height: 5rem;
 `
 
 export default HeaderStyled;
