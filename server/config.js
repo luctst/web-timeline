c3bf9d2571e509c4bb4893a68a89a1a2e1578f69
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-13 00:56:14 +0100

    Handle config data before starting the server

---
+++ server/config.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv");
+const chalk = require("chalk");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+let url;
+
+if (!envFound) {
+    throw new Error(chalk`{red ⚠️  Couldn't find .env file  ⚠️}`);
+}
+
+if (process.env.NODE_ENV === "development") {
+    url = "127.0.0.1";
+} else {
+    url = "https://apiurl.com/"; //TODO: Enter the apiurl to use for production
+};
+
+module.exports = {
+    databaseUrl: process.env.DB_URL,
+    url,
+};
