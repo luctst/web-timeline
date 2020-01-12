5da5f5cb974562946fa269dbfe3ccd51b9834520
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-13 00:55:49 +0100

    Create container global style

---
+++ styles/Container.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export default styled.section`
+    padding: 0 15px;
+    margin: 0 auto;
+
+    @media (min-width: 576px) {
+		max-width: 540px;
+	}
+	@media (min-width: 768px) {
+		max-width: 720px;
+	}
+	@media (min-width: 992px) {
+		max-width: 960px;
+	}
+	@media (min-width: 1200px) {
+		max-width: 1140px;
+	}
+`;
